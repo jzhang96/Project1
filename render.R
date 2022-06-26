391fd8dc2c593562d79fbe13b7d16f7dbf580512
jzhang96 <jzhang96@ncsu.edu>
2022-06-26 14:33:42 -0400

    update

--- render.R
+++ render.R
@@ -1,6 +1,6 @@
 rmarkdown::render("C:/Users/LAILA/Desktop/558/git/project1/Project1/README.Rmd",
                   output_format = "github_document",
-                  output_file = "C:/Users/LAILA/Desktop/558/git/project1/Project1/README.md",
+                  output_dir = "C:/Users/LAILA/Desktop/558/git/project1/Project1",
                   output_options = list(
                     html_preview = FALSE
                   ))
